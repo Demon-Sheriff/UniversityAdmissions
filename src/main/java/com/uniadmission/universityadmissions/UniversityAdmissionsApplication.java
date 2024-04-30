2ade173dc6240982e5f55dc177cff7db91e2d2ee
skushagra <kushagra.rigel@gmail.com>
2024-05-01 01:56:42 +0530

    Initial commit

---
+++ src/main/java/com/uniadmission/universityadmissions/UniversityAdmissionsApplication.java
@@ -0,0 +1,13 @@
+package com.uniadmission.universityadmissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAdmissionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityAdmissionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniadmission/universityadmissions/UniversityAdmissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniadmission.universityadmissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAdmissionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
